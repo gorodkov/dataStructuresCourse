8f9472fb2a7dd801e600b4f9240752ba4bde5998
Dmitrii Gorodkov <dgorodkov@gmail.com>
2016-05-31 16:21:18 +0300

    Create ArrayCollection

---
+++ src/exercises/collection/ArrayCollection.java
@@ -0,0 +1,111 @@
+package exercises.collection;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+public class ArrayCollection<T> implements Collection<T> {
+
+    private T[] m = (T[])new Object[1];
+
+    private int size;
+
+    @Override
+    public int size() {
+        // BEGIN (write your solution here)
+
+        // END
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // BEGIN (write your solution here)
+
+        // END
+    }
+
+    @Override
+    public boolean contains(final Object o) {
+        // BEGIN (write your solution here)
+
+        // END
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        // BEGIN (write your solution here)
+
+        // END
+    }
+
+    @Override
+    public Object[] toArray() {
+        // BEGIN (write your solution here)
+
+        // END
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        // BEGIN (write your solution here)
+
+        // END
+    }
+
+    @Override
+    public boolean add(final T t) {
+        // BEGIN (write your solution here)
+
+        // END
+    }
+
+    @Override
+    public boolean remove(final Object o) {
+        // BEGIN (write your solution here)
+
+        // END
+    }
+
+    @Override
+    public boolean containsAll(final Collection<?> c) {
+        // BEGIN (write your solution here)
+
+        // END
+    }
+
+    @Override
+    public boolean addAll(final Collection<? extends T> c) {
+        // BEGIN (write your solution here)
+
+        // END
+    }
+
+    @Override
+    public boolean removeAll(final Collection<?> c) {
+        // BEGIN (write your solution here)
+
+        // END
+    }
+
+    @Override
+    public boolean retainAll(final Collection<?> c) {
+        // BEGIN (write your solution here)
+
+        // END
+    }
+
+    @Override
+    public void clear() {
+        // BEGIN (write your solution here)
+
+        // END
+    }
+
+    private class ElementsIterator implements Iterator<T> {
+        // BEGIN (write your solution here)
+
+
+        // END
+    }
+
+}
+
