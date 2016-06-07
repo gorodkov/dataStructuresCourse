563d82da139a8b96666a215eda75d56c8ede8d4d
Dmitrii Gorodkov <dgorodkov@gmail.com>
2016-06-07 17:32:41 +0300

    Course is created.

---
+++ src/exercises/overridingEquals/Course.java
@@ -0,0 +1,65 @@
+package exercises.overridingEquals;
+
+import java.util.List;
+import java.util.Date;
+
+public class Course {
+
+    private final Long uuid;
+
+    private final String name;
+
+    private final List<Session> sessions;
+
+    public Course(final Long uuid, final String name, final List<Session> sessions) {
+        this.uuid = uuid;
+        this.name = name;
+        this.sessions = sessions;
+    }
+
+    public Long getUuid() {
+        return uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Session> getSessions() {
+        return sessions;
+    }
+
+    @Override
+    public boolean equals(final Object object) {
+        // BEGIN (write your solution here)
+
+        // END
+    }
+
+    public class Session {
+
+        private final Date startDate;
+
+        public Session(final Date startDate) {
+            this.startDate = startDate;
+        }
+
+        public Date getStartDate() {
+            return this.startDate;
+        }
+
+        public Course getCourse() {
+            return Course.this;
+        }
+
+        @Override
+        public boolean equals(final Object object) {
+            // BEGIN (write your solution here)
+
+            // END
+        }
+
+    }
+
+}
+
