d25d0b3dce1482358a8657155cc5cbc2af9cb6f2
Dmitrii Gorodkov <dgorodkov@gmail.com>
2016-06-08 11:02:22 +0300

    Course is created.

---
+++ src/exercises/overridingHashCode/Course.java
@@ -0,0 +1,89 @@
+package exercises.overridingHashCode;
+
+import java.util.List;
+import java.util.Date;
+
+public class Course {
+
+    private final String uuid;
+
+    private final String name;
+
+    private final List<Session> sessions;
+
+    public Course(final String uuid, final String name, final List<Session> sessions) {
+        this.uuid = uuid;
+        this.name = name;
+        this.sessions = sessions;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Session> getSessions() {
+        return sessions;
+    }
+
+    @Override
+    public boolean equals(final Object object) {
+        if (!(object instanceof Course))
+            return false;
+
+        final Course that = (Course) object;
+        if (!that.getUuid().equals(this.getUuid()))
+            return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        // BEGIN (write your solution here)
+
+        // END
+    }
+
+    public class Session {
+
+        private final Date startDate;
+
+        public Session(final Date startDate) {
+            this.startDate = startDate;
+        }
+
+        public Date getStartDate() {
+            return this.startDate;
+        }
+
+        public Course getCourse() {
+            return Course.this;
+        }
+
+        @Override
+        public boolean equals(final Object object) {
+            if (!(object instanceof Session)) return false;
+
+            final Session that = (Session) object;
+            if (!that.getStartDate().equals(this.getStartDate())) return false;
+
+            if (!that.getCourse().equals(this.getCourse())) return false;
+
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            // BEGIN (write your solution here)
+
+            // END
+        }
+
+    }
+
+}
+
