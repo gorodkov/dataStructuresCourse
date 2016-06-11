4f19f7c532f9832e35e9baeb105dfb7b74d55c9a
Dmitrii Gorodkov <dgorodkov@gmail.com>
2016-06-11 21:23:55 +0300

    HashSet is created.

---
+++ src/exercises/hashSet/HashSet.java
@@ -0,0 +1,103 @@
+package exercises.hashSet;
+
+import java.util.*;
+
+public class HashSet<T> implements Set<T> {
+
+    private static final Boolean EXIST = true;
+
+    private final Map<T, Boolean> elements = new HashMap<>();
+
+    @Override
+    public int size() {
+        // BEGIN (write your solution here)
+        return map.size();
+        // END
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // BEGIN (write your solution here)
+        return map.isEmpty();
+        // END
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        // BEGIN (write your solution here)
+        return map.containsKey(o);
+        // END
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        // BEGIN (write your solution here)
+        return map.keySet().iterator();
+        // END
+    }
+
+    @Override
+    public Object[] toArray() {
+        // BEGIN (write your solution here)
+
+        // END
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        // BEGIN (write your solution here)
+
+        // END
+    }
+
+    @Override
+    public boolean add(T t) {
+        // BEGIN (write your solution here)
+
+        // END
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        // BEGIN (write your solution here)
+
+        // END
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        // BEGIN (write your solution here)
+
+        // END
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        // BEGIN (write your solution here)
+
+        // END
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        // BEGIN (write your solution here)
+
+        // END
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        // BEGIN (write your solution here)
+
+        // END
+    }
+
+    @Override
+    public void clear() {
+        // BEGIN (write your solution here)
+
+        // END
+    }
+
+}
+
