7d85596c52b8ddd71a99bd53efc430ad020f07de
Dmitrii Gorodkov <dgorodkov@gmail.com>
2016-06-12 21:58:08 +0300

    HashSet methods are added. Test are passed.

--- src/exercises/hashSet/HashSet.java
+++ src/exercises/hashSet/HashSet.java
@@ -18,93 +18,85 @@ public class HashSet<T> implements Set<T> {
 
     @Override
     public int size() {
-        // BEGIN (write your solution here)
-        return map.size();
-        // END
+        return elements.size();
     }
 
     @Override
     public boolean isEmpty() {
-        // BEGIN (write your solution here)
-        return map.isEmpty();
-        // END
+        return elements.isEmpty();
     }
 
     @Override
     public boolean contains(Object o) {
-        // BEGIN (write your solution here)
-        return map.containsKey(o);
-        // END
+        return elements.containsKey(o);
     }
 
     @Override
     public Iterator<T> iterator() {
-        // BEGIN (write your solution here)
-        return map.keySet().iterator();
-        // END
+        return elements.keySet().iterator();
     }
 
     @Override
     public Object[] toArray() {
-        // BEGIN (write your solution here)
-
-        // END
+        return elements.keySet().toArray();
     }
 
     @Override
     public <T1> T1[] toArray(T1[] a) {
-        // BEGIN (write your solution here)
-
-        // END
+        return elements.keySet().toArray(a);
     }
 
     @Override
     public boolean add(T t) {
-        // BEGIN (write your solution here)
-
-        // END
+        elements.put(t, EXIST);
+        return true;
     }
 
     @Override
     public boolean remove(Object o) {
-        // BEGIN (write your solution here)
-
-        // END
+        elements.remove(o);
+        return true;
     }
 
     @Override
     public boolean containsAll(Collection<?> c) {
-        // BEGIN (write your solution here)
-
-        // END
+        for (Object e : c) {
+            if (!contains(e)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     @Override
     public boolean addAll(Collection<? extends T> c) {
-        // BEGIN (write your solution here)
-
-        // END
+        boolean modified = false;
+        for (T e : c) {
+            if (add(e)) {
+                modified = true;
+            }
+        }
+        return modified;
     }
 
     @Override
     public boolean removeAll(Collection<?> c) {
-        // BEGIN (write your solution here)
-
-        // END
+        for (Object o : c)
+            remove(o);
+        return true;
     }
 
     @Override
     public boolean retainAll(Collection<?> c) {
-        // BEGIN (write your solution here)
-
-        // END
+        for (Object o : this)
+            if (!c.contains(o))
+                remove(o);
+        return true;
     }
 
     @Override
     public void clear() {
-        // BEGIN (write your solution here)
-
-        // END
+        elements.clear();
     }
 
 }
